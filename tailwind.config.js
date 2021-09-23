ce72e99a1799dbcdf958d8640020e474aada2570
Daniel Vasic <vsc84@outlook.de>
2021-09-23 11:12:51 +0200

    main

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const colors = require('tailwindcss/colors')
+
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        beige: '#fbf4e1',
+        brown: '#4f3c35',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
